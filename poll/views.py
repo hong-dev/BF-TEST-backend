c09e96a8a03e9a814524bd8e9dabfc4e8c0163d7
kay <k904808@gmail.com>
2020-04-23 16:40:50 +0900

    Create QuestionView and Set url

--- poll/views.py
+++ poll/views.py
@@ -1,4 +1,8 @@
 from django.views import View
 from django.http  import HttpResponse, JsonResponse
 
-class 
+class QuestionView(View):
+    def get(self, request, question_id):
+        questions = Question.objects.filter(id = question_id).values()
+
+        return JsonResponse({"question" : quetions[0]}, status = 200)
