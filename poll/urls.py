e59b61ccbdcc2af3352a77d6ee92febebf431eb8
hong-dev <jjuggumih@gmail.com>
2020-04-24 15:54:31 +0900

    result view

--- poll/urls.py
+++ poll/urls.py
@@ -1,6 +1,7 @@
-from .views      import QuestionView
+from .views      import QuestionView, ResultView
 from django.urls import path
 
 urlpatterns = [
     path('/<int:question_id>', QuestionView.as_view()),
+    path('/result', ResultView.as_view())
 ]
--- poll/views.py
+++ poll/views.py
@@ -36,33 +36,66 @@ class QuestionView(View):
             'choice'    : [
                 {
                     'id'     : choice["id"],
-                    'choice' : choice["choice"]
+                    'choice' : choice["choice"],
                 } for choice in choices ]
         }
 
         return JsonResponse({"question_data" : question_data}, status = 200)
 
-#class ResultView(View):
-#    def post(self, request):
-#        try:
-#            score  = json.loads(request.body)["answer"]
-#            p_type = json.loads(request.body)["type"]
-#            browser = request.META['HTTP_USER_AGENT']
-#            ip = request.META.get('HTTP_X_FORWARDED_FOR')
-#
-#            Response = (
-#                response = score,
-#                case = p_type,
-#                browser = browser,
-#                ip_address = ip
-#            ).save()
-#
-#            if sum(score) == len(score)/2:
-#                result = Result.objects.filter(id=4).values()
-#
-#            elif sum(score) > len(score)/2:
-#
-#            elif sum(score) < len(score)/2:
-#
-#
-#        return JsonResponse({"question" : quetions[0]}, status = 200)
+class ResultView(View):
+    def post(self, request):
+        try:
+            score      = json.loads(request.body)["answer"]
+            p_type     = json.loads(request.body)["type"]
+            user_name  = json.loads(request.body)["user"]
+            browser    = request.META['HTTP_USER_AGENT']
+            ip_address = request.META.get('HTTP_X_FORWARDED_FOR')
+
+            user = User(
+                name       = user_name,
+                browser    = browser,
+                ip_address = ip_address
+            )
+
+            count_front = 0
+            count_valid_question = 0
+            for question_id, choice_id in score.items():
+                Response(
+                    user     = user,
+                    question = question_id,
+                    choice   = answer_id
+                ).save()
+
+                if Choice.objects.select_related('stack').get(id = choice_id).stack == 1:
+                    count_front += 1
+
+                if Choice.objects.select_related('stack').get(id = choice_id).stack != null:
+                    count_valid_question += 1
+
+            if count_front == count_valid_question/2:
+                result  = Result.objects.get(stack = 3)
+                dev_fit = Result.objects.get(stack = 3)
+
+            elif count_front > count_valid_question/2:
+                result  = Result.objects.get(case = p_type, stack = 1)
+                dev_fit = Result.objects.get(case = p_type, stack = 2)
+
+            else:
+                result  = Result.objects.get(case = p_type, stack = 2)
+                dev_fit = Result.objects.get(case = p_type, stack = 1)
+
+            user.result_id = result
+            user.save()
+
+            user_result = {
+                'name'        : result.name,
+                'description' : result.description,
+                'image_url'   : result.image_url,
+                'audio_url'   : result.audio_url,
+                'dev_fit'     : dev_fit.name
+            }
+
+            return JsonResponse({"result" : user_result}, status = 200)
+
+        except KeyError:
+            return JsonResponse({"error" : "INVALID_KEYS"}, status = 400)
