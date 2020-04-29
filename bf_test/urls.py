5546c8e469b1e0bdc3f205f521eba24d38487966
whybein <swhybeing@gmail.com>
2020-04-29 18:06:16 +0900

    feat ping

--- bf_test/urls.py
+++ bf_test/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('poll', include('poll.urls')),
+    path('ping', include('poll.urls')),
 ]
--- poll/urls.py
+++ poll/urls.py
@@ -1,7 +1,8 @@
-from .views      import QuestionView, ResultView
+from .views      import QuestionView, ResultView, PingView
 from django.urls import path
 
 urlpatterns = [
     path('/<int:question_id>', QuestionView.as_view()),
-    path('/result', ResultView.as_view())
+    path('/result', ResultView.as_view()),
+    path('', PingView.as_view()),
 ]
